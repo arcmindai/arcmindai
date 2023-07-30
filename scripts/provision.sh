1ed2d80521e55fea680c9b372478ce21fef850f9
Henry Chan <henry@kinwo.net>
2023-07-30 17:26:54 +1000

    feat(brain) add arcmindai_brain canister
    * add provision script to deploy primary (controller) and secondary (brain) with primary controller as owner of the brain canister

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,4 +1,13 @@
 MyPrincipal=$(dfx identity get-principal)
-echo $MyPrincipal
-# dfx deploy arcmindai_controller
+
+echo Deploying controller canister with principal $MyPrincipal
 dfx deploy arcmindai_controller --argument "(opt principal \"$MyPrincipal\")"
+echo Controller Owner:
+dfx canister call arcmindai_controller get_owner
+
+ControllerPrincipal=$(dfx canister id arcmindai_controller)
+echo Deploying brain canister with controller principal $ControllerPrincipal
+dfx deploy arcmindai_brain --argument "(opt principal \"$ControllerPrincipal\")"
+
+echo Brain Owner:
+dfx canister call arcmindai_brain get_owner
--- scripts/query.sh
+++ scripts/query.sh
@@ -1,2 +1,3 @@
 # dfx canister call arcmindai_controller greet '("Henry")'
 dfx canister call arcmindai_controller get_owner
+dfx canister call arcmindai_brain get_owner
