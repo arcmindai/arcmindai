36f6978c2f5f5552c0a79484732551a749cee958
Henry Chan <henry@kinwo.net>
2023-08-05 17:22:03 +1000

    doc(arcmind) add README with quick start, intro

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,10 +1,12 @@
 MyPrincipal=$(dfx identity get-principal)
 
+# Deploy the controller canister
 echo Deploying controller canister with principal $MyPrincipal
 dfx deploy arcmindai_controller --argument "(opt principal \"$MyPrincipal\")"
 echo Controller Owner:
 dfx canister call arcmindai_controller get_owner
 
+# Deploy the brain canister with controller as owner
 ControllerPrincipal=$(dfx canister id arcmindai_controller)
 echo Deploying brain canister with controller principal $ControllerPrincipal
 dfx deploy arcmindai_brain --argument "(opt principal \"$ControllerPrincipal\")"
