cb58b9e8376b16f165467b80b1eaed1b14a3eea3
Henry Chan <henry@kinwo.net>
2023-08-06 17:34:56 +1000

    feat(openai) implement OpenAI Completion API call in brain canister using HTTP Post outcall
    * allow OpenAI API call from controller only with ask function
    * add test script query to ask question
    * update provision script

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,3 +1,5 @@
-# dfx canister call arcmindai_controller greet '("Henry")'
-dfx canister call arcmindai_controller get_owner
-dfx canister call arcmindai_brain get_owner
+# dfx canister call arcmindai_controller ask '("Henry")'
+# dfx canister call arcmindai_brain ask '("I am Henry Chan. What is my first name?")'
+dfx canister call arcmindai_controller ask '("I am Henry Chan. What is my first name?")'
+# dfx canister call arcmindai_controller get_brain_canister
+# dfx canister call arcmindai_brain get_owner
--- src/arcmindai_controller/src/lib.rs
+++ src/arcmindai_controller/src/lib.rs
@@ -16,6 +16,7 @@ mod guards;
 #[derive(Default, CandidType, Serialize, Deserialize)]
 pub struct State {
     pub owner: Option<Principal>,
+    pub brain_canister: Option<Principal>,
 }
 
 // Mutable global state
@@ -23,14 +24,29 @@ thread_local! {
     static STATE: RefCell<State> = RefCell::default();
 }
 
+// ---------------------- ArcMind AI Agent ----------------------
+// entry function for user to ask questions
+#[update(guard = "assert_owner")]
+#[candid_method(update)]
+async fn ask(question: String) -> String {
+    let brain_canister: Principal = STATE.with(|state| (*state.borrow()).brain_canister.unwrap());
+    let (result,): (String,) = ic_cdk::api::call::call(brain_canister, "ask", (question,))
+        .await
+        .expect("call to ask failed");
+
+    return result;
+}
+
+// ---------------------- Supporting Functions ----------------------
 // Controller canister must be created with principal
 #[init]
 #[candid_method(init)]
-fn init(owner: Option<Principal>) {
+fn init(owner: Option<Principal>, brain_canister: Option<Principal>) {
     let my_owner: Principal = owner.unwrap_or_else(|| api::caller());
     STATE.with(|state| {
         *state.borrow_mut() = State {
             owner: Some(my_owner),
+            brain_canister: brain_canister,
         };
     });
 }
@@ -41,12 +57,19 @@ pub fn get_owner() -> Option<Principal> {
     STATE.with(|state| (*state.borrow()).owner)
 }
 
+#[query]
+#[candid_method(query)]
+pub fn get_brain_canister() -> Option<Principal> {
+    STATE.with(|state| (*state.borrow()).brain_canister)
+}
+
 #[update(guard = "assert_owner")]
 #[candid_method(update)]
 pub fn update_owner(new_owner: Principal) {
     STATE.with(|state| {
         *state.borrow_mut() = State {
             owner: Some(new_owner),
+            brain_canister: state.borrow().brain_canister,
         };
     });
 }
@@ -67,6 +90,7 @@ fn post_upgrade() {
     })
 }
 
+// ---------------------- Candid declarations did file generator ----------------------
 #[cfg(test)]
 mod tests {
     use candid::{export_service, Principal};
