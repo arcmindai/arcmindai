ecff41467a42a6104d12e45d49646e38f3f0a580
Henry Chan <henry@kinwo.net>
2023-10-29 14:17:20 +1000

    ci - add missing ENV VARS

--- scripts/deploy_tools.sh
+++ scripts/deploy_tools.sh
@@ -4,5 +4,5 @@ IC_NETWORK=${IC_NETWORK:-local}
 OWENR_PRINCIPAL=$(dfx identity --network $IC_NETWORK get-principal)
 
 # Deploy tools canister
-echo Deploying tools canister with owner $OWENR_PRINCIPAL on $IC_NETWORK, GOOGLE_API_KEY=$GOOGLE_API_KEY, GOOGLE_SEARCH_ENGINE_ID=$GOOGLE_SEARCH_ENGINE_ID
+echo Deploying tools canister with owner=$OWENR_PRINCIPAL on $IC_NETWORK, GOOGLE_API_KEY=$GOOGLE_API_KEY, GOOGLE_SEARCH_ENGINE_ID=$GOOGLE_SEARCH_ENGINE_ID
 dfx deploy --network $IC_NETWORK arcmindai_tools --argument "(opt principal \"$OWENR_PRINCIPAL\", \"$GOOGLE_API_KEY\", \"$GOOGLE_SEARCH_ENGINE_ID\")"
