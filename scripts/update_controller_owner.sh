9edcfdeeed7d636ffcd5b4deb8433bd85914641a
Henry Chan <henry@kinwo.net>
2023-12-28 17:27:54 +1000

    chore - add validate CONTROLLER_OWNER env var

--- scripts/update_controller_owner.sh
+++ scripts/update_controller_owner.sh
@@ -1,9 +1,12 @@
-# Update controller owner
+# Validate required env vars
+if [[ -z "${CONTROLLER_OWNER}" ]]; then
+  echo "CONTROLLER_OWNER is unset."
+  exit 1
+fi
 
 # To deplopy locally, update IC_NETWORK to local. To deploy to ic, update IC_NETWORK to ic.
 IC_NETWORK=${IC_NETWORK:-local}
 
-CONTROLLER_OWNER=og5ck-wstph-2m7s5-enjzf-7heh5-ko754-eoaj4-n2ccx-imtiu-s4ieb-4ae
 echo Updating controller owner to controller $CONTROLLER_OWNER
 dfx canister --network $IC_NETWORK call arcmindai_controller update_owner "(principal \"$CONTROLLER_OWNER\")"
 
