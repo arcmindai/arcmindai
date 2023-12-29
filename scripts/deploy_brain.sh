bb386f0218e327d1df60284d8dc1ee6f5aa9ff1c
Henry Chan <henry@kinwo.net>
2023-12-29 15:37:20 +1000

    build - refactor script variable name to use the naming convention
    * update provision deploy owner and use VECTOR_PRINCIPAL from arcmindvector provision script

--- scripts/deploy_brain.sh
+++ scripts/deploy_brain.sh
@@ -9,6 +9,7 @@ IC_NETWORK=${IC_NETWORK:-local}
 GPT_MODEL=gpt-4
 
 # Deploy brain canister 
-ControllerPrincipal=$(dfx canister --network $IC_NETWORK id arcmindai_controller)
-echo Deploying brain canister with owner=$ControllerPrincipal, GPT model=$GPT_MODEL and OPENAI_API_KEY=$OPENAI_API_KEY on $IC_NETWORK
-dfx deploy --network $IC_NETWORK arcmindai_brain --argument "(opt principal \"$ControllerPrincipal\", \"$OPENAI_API_KEY\", \"$GPT_MODEL\")"
+CONTROLLER_PRINCIPAL=$(dfx canister --network $IC_NETWORK id arcmindai_controller)
+
+echo Deploying brain canister with owner=$CONTROLLER_PRINCIPAL, GPT model=$GPT_MODEL and OPENAI_API_KEY=$OPENAI_API_KEY on $IC_NETWORK
+dfx deploy --network $IC_NETWORK arcmindai_brain --argument "(opt principal \"$CONTROLLER_PRINCIPAL\", \"$OPENAI_API_KEY\", \"$GPT_MODEL\")"
