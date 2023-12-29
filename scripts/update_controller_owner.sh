21222e1d1c9966aabb15be970820cad9bad92bf0
Henry Chan <henry@kinwo.net>
2023-12-29 16:41:49 +1000

    build - add new GitHub Action workflows for provisioning new suite of ArcMind canisters and assign new owner

--- scripts/update_controller_owner.sh
+++ scripts/update_controller_owner.sh
@@ -4,11 +4,16 @@ if [[ -z "${CONTROLLER_OWNER}" ]]; then
   exit 1
 fi
 
+if [[ -z "${CONTROLLER_CANISTER_ID}" ]]; then
+  echo "CONTROLLER_CANISTER_ID is unset."
+  exit 1
+fi
+
 # To deplopy locally, update IC_NETWORK to local. To deploy to ic, update IC_NETWORK to ic.
 IC_NETWORK=${IC_NETWORK:-local}
 
-echo Updating controller owner to controller $CONTROLLER_OWNER
-dfx canister --network $IC_NETWORK call arcmindai_controller update_owner "(principal \"$CONTROLLER_OWNER\")"
+echo Updating controller canister $CONTROLLER_CANISTER_ID owner to $CONTROLLER_OWNER
+dfx canister --network $IC_NETWORK call $CONTROLLER_CANISTER_ID update_owner "(principal \"$CONTROLLER_OWNER\")"
 
 echo controller Owner:
-dfx canister --network $IC_NETWORK call arcmindai_controller get_owner
+dfx canister --network $IC_NETWORK call $CONTROLLER_CANISTER_ID get_owner
