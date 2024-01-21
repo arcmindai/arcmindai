da65366496af3fbf5054274ad5915daf166feb83
Henry Chan <henry@kinwo.net>
2024-01-21 18:05:33 +1000

    feat(controller) add biling_key, get_max_num_thoughts_allowed, get_num_thoughts_processed
    * inc_max_num_thoughts_limit
    * is_exceed_max_num_thoughts_allowed
    * store paymen transaction in stablevec stable_paymenttransaction_data
    * track max_num_thoughts_allowed, num_thoughts_processed
    * expose http /inc_max_num_thoughts_limit

--- scripts/deploy_controller.sh
+++ scripts/deploy_controller.sh
@@ -16,6 +16,11 @@ if [[ -z "${BEAMFI_PRINCIPAL}" ]]; then
   exit 1
 fi
 
+if [[ -z "${BILLING_KEY}" ]]; then
+  echo "BILLING_KEY is unset."
+  exit 1
+fi
+
 # To deplopy locally, update IC_NETWORK to local. To deploy to ic, update IC_NETWORK to ic.
 IC_NETWORK=${IC_NETWORK:-local}
 
@@ -24,5 +29,5 @@ TOOLS_PRINCIPAL=$(dfx canister --network $IC_NETWORK id arcmindai_tools)
 BROWSE_WEBSITE_GPT_MODEL=gpt-3.5-turbo-1106
 
 # Deploy controller canister
-echo Deploying controller canister with owner=$OWNER_PRINCIPAL, brain=$BRAIN_PRINCIPAL, browse_website_gpt_model=$BROWSE_WEBSITE_GPT_MODEL, tools=$TOOLS_PRINCIPAL, VECTOR_PRINCIPAL=$VECTOR_PRINCIPAL, BEAMFI_PRINCIPAL=$BEAMFI_PRINCIPAL on $IC_NETWORK
-dfx deploy --network $IC_NETWORK arcmindai_controller --argument "(opt principal \"$OWNER_PRINCIPAL\", opt principal \"$BRAIN_PRINCIPAL\", opt principal \"$TOOLS_PRINCIPAL\", opt principal \"$VECTOR_PRINCIPAL\", opt principal \"$BEAMFI_PRINCIPAL\", opt \"$BROWSE_WEBSITE_GPT_MODEL\")"
+echo Deploying controller canister with owner=$OWNER_PRINCIPAL, brain=$BRAIN_PRINCIPAL, browse_website_gpt_model=$BROWSE_WEBSITE_GPT_MODEL, tools=$TOOLS_PRINCIPAL, VECTOR_PRINCIPAL=$VECTOR_PRINCIPAL, BEAMFI_PRINCIPAL=$BEAMFI_PRINCIPAL, BILLING_KEY=$BILLING_KEY on $IC_NETWORK
+dfx deploy --network $IC_NETWORK arcmindai_controller --argument "(opt principal \"$OWNER_PRINCIPAL\", opt principal \"$BRAIN_PRINCIPAL\", opt principal \"$TOOLS_PRINCIPAL\", opt principal \"$VECTOR_PRINCIPAL\", opt principal \"$BEAMFI_PRINCIPAL\", opt \"$BROWSE_WEBSITE_GPT_MODEL\", opt \"$BILLING_KEY\")"
