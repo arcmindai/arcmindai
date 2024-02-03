dc4e488133b4ec2afd996d9e95fb18199c35f5c2
Henry Chan <henry@kinwo.net>
2024-02-03 14:54:26 +1000

    doc(sample) add good questions

--- interact/query.sh
+++ interact/query.sh
@@ -1,4 +1,6 @@
 # dfx canister call arcmindai_controller get_brain_canister
 # dfx canister call arcmindai_brain get_owner
 
-dfx canister call arcmindai_controller get_goal 0 --query
+# dfx canister call arcmindai_controller get_goal 0 --query
+
+dfx canister call arcmindai_controller get_num_thoughts_processed --query
